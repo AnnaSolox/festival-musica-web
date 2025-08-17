83055a045bf1e2ca5c588456caad5c368298e11e
AnnaSolox <anna.sol.loz@gmail.com>
2025-08-17 15:24:19 +0000

    lazy loading and thumb images

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
+import path from 'path'
+import fs from 'fs'
 import { src, dest, watch, series } from 'gulp' // importar funciones de gulp
 import * as dartSass from 'sass' // importar sass
 import gulpSass from 'gulp-sass' // importar gulp-sass
 import terser from 'gulp-terser' // minificar js
+import sharp from 'sharp'
 
 const sass = gulpSass(dartSass); //enlazamos gulp con sas
 
@@ -23,10 +26,40 @@ export function css(done) {
     done();
 }
 
+
+// Codigo de node.js para hacer las imágenes más pequeñas --> sharp + fs
+export async function crop(done) {
+    const inputFolder = 'src/img/gallery/full'
+    const outputFolder = 'src/img/gallery/thumb';
+    const width = 250;
+    const height = 180;
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdirSync(outputFolder, { recursive: true })
+    }
+    const images = fs.readdirSync(inputFolder).filter(file => {
+        return /\.(jpg)$/i.test(path.extname(file));
+    });
+    try {
+        images.forEach(file => {
+            const inputFile = path.join(inputFolder, file)
+            const outputFile = path.join(outputFolder, file)
+            sharp(inputFile) 
+                .resize(width, height, {
+                    position: 'centre'
+                })
+                .toFile(outputFile)
+        });
+
+        done()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export function dev() { // no pasamos el done para que no termine y siga escuchando por cambios
     watch('src/scss/**/*.scss', css); // observará el archivo scss y ejecutará la función css cada vezç
     watch('src/js/**/*.js', js);
 }
 
 // Inicializar todas las tareas cuando se abre la página
-export default series(js, css, dev);
+export default series(crop, js, css, dev);
