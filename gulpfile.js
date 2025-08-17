0af69952dda4965432d9b1eea8ff26deff2c76fb
AnnaSolox <anna.sol.loz@gmail.com>
2025-08-17 15:12:23 +0000

    minimize css js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 import { src, dest, watch, series } from 'gulp' // importar funciones de gulp
 import * as dartSass from 'sass' // importar sass
 import gulpSass from 'gulp-sass' // importar gulp-sass
+import terser from 'gulp-terser' // minificar js
 
 const sass = gulpSass(dartSass); //enlazamos gulp con sas
 
 export function js(done) {
     src('src/js/app.js')
+        .pipe(terser())
         .pipe(dest('build/js'));
 
     done();
@@ -13,7 +15,9 @@ export function js(done) {
 
 export function css(done) {
     src('src/scss/app.scss', { sourcemaps: true }) // source
-        .pipe(sass().on('error', sass.logError)) // aplicar sass con la relación hecha previamente
+        .pipe(sass({
+            style: 'compressed'
+        }).on('error', sass.logError)) // aplicar sass con la relación hecha previamente
         .pipe(dest('build/css', { sourcemaps: '.' })) // carpeta destino
 
     done();
@@ -25,4 +29,4 @@ export function dev() { // no pasamos el done para que no termine y siga escucha
 }
 
 // Inicializar todas las tareas cuando se abre la página
-export default series (js, css, dev);
+export default series(js, css, dev);
